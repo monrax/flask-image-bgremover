b09b53d8fbb0ff8ec5496aa9f49b5a1266689ac1
Ramon Marquez <ramon.marquez@graylog.com>
2024-06-20 14:56:09 -0400

    feat: new endpoint to edit image before bg removal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from utils.background import remove_bg_image
+from utils.background import remove_bg_image, edit_and_remove_bg
 
 
 app = Flask(__name__)
@@ -16,6 +16,24 @@ def remove_bg():
     if image is not None:
         result = remove_bg_image(
             image, 
+            to_base64=True, 
+            myme=True,
+            _format="png"
+        )
+        response = {
+            "result": result
+        }
+        return response
+    return "Something went wrong!"
+
+@app.route("/editandremovebg", methods=["POST"])
+def edit_im_remove_bg():
+    image = request.json.get("image")
+    expression = request.json.get("expr")
+    if image is not None:
+        result = edit_and_remove_bg(
+            image, 
+            expression,
             to_base64=True, 
             myme=True,
             _format="png"
--- utils/background.py
+++ utils/background.py
@@ -1,6 +1,6 @@
 from typing import Union
 from rembg import remove
-from PIL import Image
+from PIL import Image, ImageMath
 
 # base64 image utils
 from utils.images import base64_to_image, image_to_base64
@@ -19,3 +19,19 @@ def remove_bg_image(
     if to_base64:
         result = image_to_base64(result, _format=_format, myme=myme)
     return result
+
+def edit_and_remove_bg(
+    image: Union[str, Image.Image],
+    expression: str,
+    *args, **kwargs
+):
+    """applies "image expression" to edit image, then removes image bg"""
+    if expression is None:
+       return remove_bg_image(image, *args, **kwargs)
+    if isinstance(image, str):
+      image = base64_to_image(image)
+    try:
+      edited = ImageMath.eval(expression, image=image)
+    except ValueError:
+      pass
+    return remove_bg_image(image if edited is None else edited, *args, **kwargs)
